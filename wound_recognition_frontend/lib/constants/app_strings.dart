d5ea203dbf59a49c22cc5a973e51da5c40e7f60e
Koen Benjamin Sikkema <koensikkema@hotmail.com>
2025-05-27 10:44:37 +0200

    [FIX] changed appicon and appname

--- wound_recognition_frontend/lib/constants/app_strings.dart
+++ wound_recognition_frontend/lib/constants/app_strings.dart
@@ -5,7 +5,8 @@ class AppStrings{
   static const String resultPage = 'Resultaat Pagina';
   static const String homePage = 'Home Pagina';
   static const String uploadButton = 'Afbeelding Uploaden';
-  static const String appName = 'WondherkenningsApplicatie';
+  static const String appName = 'WondherkenningsApp';
   static const String chooseImage = 'Selecteren uit Galerij';
-  static const String predictionsPage = 'Alle voorspellingen';
+  static const String predictionsPage = 'Lokale Voorspellingen';
+  static const String historyPage = 'Geschiedenis Ophalen';
 }
